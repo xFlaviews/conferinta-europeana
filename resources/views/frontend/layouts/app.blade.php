b2441c74fe078bcc5af12e960d9b3283cc6505a3
xFlaviews <155723308+xFlaviews@users.noreply.github.com>
2024-07-13 16:53:21 +0200

    feat(favicon)/added-favicon (#6)
    Co-authored-by: fmiron <fmiron@sorint.com>

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -20,6 +20,13 @@
     @vite(['resources/sass/app.scss'])
 
     @stack('after-styles')
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1b264f">
+    <meta name="msapplication-TileColor" content="#1b264f">
+    <meta name="theme-color" content="#ffffff">
 </head>
 
 <body>
