f11a8da50446627f9c05aa5f6ebc7007b3c44838
fmiron <fmiron@sorint.com>
2024-08-27 15:45:40 +0200

    fix/faq-text

--- resources/views/frontend/components/faq-section.blade.php
+++ resources/views/frontend/components/faq-section.blade.php
@@ -8,35 +8,35 @@
                 <div class="mb-3 accent-text mt-5">INTREBĂRI GENERALE</div>
             </div>
             <div class="col-12 col-md-5 text-start text-md-end mail-hyphen mb-5">
-                <span class="paragraph-regular-20">Daca ai intrebari, poti gasi raspunsuri aici.
-                    In casul in care nu gasesti raspuns la intrebarea ta,
-                    <a href="#" class="text-decoration-underline">scrie-ne</a></span>
+                <span class="paragraph-regular-20">Dacă ai întrebări, poți găsi răspunsuri aici.
+                    În cazul în care nu găsești răspuns la întrebarea ta,
+                    <a href="#" class="text-decoration-underline"> scrie-ne!</a></span>
             </div>
         </div>
 
 
-      <div class="pb-5 mb-5">
-        <div class="row justify-content-end">
-            <div class="col-12 col-md-6">
-                <div class="d-flex justify-content-between align-items-center text-start faq-items py-3">
-                    <span class="left-item fw-semibold text-regular-20">INTREBARE</span>
-                    <span class="right-item"><i class="ph ph-plus"></i></span>
-                </div>
-                <div class="d-flex justify-content-between align-items-center text-start faq-items py-3">
-                    <span class="left-item fw-semibold text-regular-20">INTREBARE</span>
-                    <span class="right-item"><i class="ph ph-plus"></i></span>
-                </div>
-                  <div class="d-flex justify-content-between align-items-center text-start faq-items py-3">
-                    <span class="left-item fw-semibold text-regular-20">INTREBARE</span>
-                    <span class="right-item"><i class="ph ph-plus"></i></span>
-                </div>
-                 <div class="d-flex justify-content-between align-items-center text-start faq-items py-3">
-                    <span class="left-item fw-semibold text-regular-20">INTREBARE</span>
-                    <span class="right-item"><i class="ph ph-plus"></i></span>
+        <div class="pb-5 mb-5">
+            <div class="row justify-content-end">
+                <div class="col-12 col-md-6">
+                    <div class="d-flex justify-content-between align-items-center text-start faq-items py-3">
+                        <span class="left-item fw-semibold text-regular-20">INTREBARE</span>
+                        <span class="right-item"><i class="ph ph-plus"></i></span>
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center text-start faq-items py-3">
+                        <span class="left-item fw-semibold text-regular-20">INTREBARE</span>
+                        <span class="right-item"><i class="ph ph-plus"></i></span>
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center text-start faq-items py-3">
+                        <span class="left-item fw-semibold text-regular-20">INTREBARE</span>
+                        <span class="right-item"><i class="ph ph-plus"></i></span>
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center text-start faq-items py-3">
+                        <span class="left-item fw-semibold text-regular-20">INTREBARE</span>
+                        <span class="right-item"><i class="ph ph-plus"></i></span>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
 
     </div>
